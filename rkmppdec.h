92840fd13b573092451235c77d2a8a5f702e1f4b
ald2004 <yuanpu@139.com>
2022-08-10 19:01:11 +0800

    Create rkmppdec.h

---
+++ rkmppdec.h
@@ -0,0 +1,75 @@
+
+#include <thread>
+#ifdef _WIN32
+//Windows
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libavutil/imgutils.h"
+};
+#else
+// linux
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+// #include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+// #include <libswscale/swscale.h>
+// #include <libavutil/imgutils.h>
+
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#include <drm_fourcc.h>
+#include <pthread.h>
+#include <rockchip/mpp_buffer.h>
+#include <rockchip/rk_mpi.h>
+#include <time.h>
+#include <unistd.h>
+#endif
+
+#define RECEIVE_FRAME_TIMEOUT   100
+#define FRAMEGROUP_MAX_FRAMES   16
+#define INPUT_MAX_PACKETS       4
+
+typedef struct {
+    MppCtx ctx;
+    MppApi *mpi;
+    MppBufferGroup frame_group;
+
+    char first_packet;
+    char eos_reached;
+
+    AVBufferRef *frames_ref;
+    AVBufferRef *device_ref;
+} RKMPPDecoder;
+
+typedef struct {
+    AVClass *av_class;
+    AVBufferRef *decoder_ref;
+} RKMPPDecodeContext;
+
+typedef struct {
+    MppFrame frame;
+    AVBufferRef *decoder_ref;
+} RKMPPFrameContext;
+
+static MppCodingType rkmpp_get_codingtype(AVCodecContext *avctx)
+{
+    switch (avctx->codec_id) {
+    case AV_CODEC_ID_H264:          return MPP_VIDEO_CodingAVC;
+    case AV_CODEC_ID_HEVC:          return MPP_VIDEO_CodingHEVC;
+    case AV_CODEC_ID_VP8:           return MPP_VIDEO_CodingVP8;
+    case AV_CODEC_ID_VP9:           return MPP_VIDEO_CodingVP9;
+    default:                        return MPP_VIDEO_CodingUnused;
+    }
+}
+
+static int rkmpp_init_decoder(AVCodecContext *avctx);
+
+
